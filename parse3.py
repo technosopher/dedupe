ed7e26b3e183a85ab40bfd8f2ac3377fd91110c7
Jordan McCarthy <mccarthy@opentechinstitute.org>
2016-11-01 09:50:24 -0400

    Added CSV output of cleaned set and conflict detection dict

--- parse3.py
+++ parse3.py
@@ -4,9 +4,9 @@
 #TODO: Write out all the dicts as separate CSVs
 
 import csv
-counter_dict = {}
 deduped_dict={}
 dup_dict={}
+conflict_dict = {}
 master_lines={}
 linecounter = 0
 fieldsets_to_match = [[5],[0,1,5],[6,7,8,9,10],[12],[0,1,13,14,15,16,17]]
@@ -18,13 +18,18 @@ with open("full_set.csv", "rb") as csvfile:
    token = ""
    for field in fieldset:
     token = token+this_row[field]
-   if len(token) > 1 and counter_dict.get(token):
-    counter_dict[token] = counter_dict[token]+1
+   if len(token) > 1 and deduped_dict.get(token):
     original_row = deduped_dict[token]
     merged_row = original_row[1]
+    if conflict_dict.get(original_row[0]):
+     conflict_dict[linecounter] = conflict_dict[original_row[0]]
+    else:
+     conflict_dict[linecounter] = {}
     for i in range(0,len(this_row)):
      if len(merged_row[i]) < 1 and len(this_row[i]) > 1:
       merged_row[i] = this_row[i]
+     elif len(merged_row[i]) > 0 and len(this_row[i]) > 0 and merged_row[i] != this_row[i]:
+      conflict_dict[linecounter][i] = this_row[i]
     deduped_dict[token] = [linecounter,merged_row]
     master_lines.pop(original_row[0])
     master_lines[linecounter] = merged_row
@@ -36,7 +41,6 @@ with open("full_set.csv", "rb") as csvfile:
      dup_dict[token] = [original_row,[linecounter,this_row]]
     break
    else:
-    counter_dict[token] = 1
     deduped_dict[token] = [linecounter,this_row]
     break
   if not master_lines.get(linecounter):
@@ -44,21 +48,28 @@ with open("full_set.csv", "rb") as csvfile:
  
 final_counter = 0
 max_counter = 0
-for key, value in counter_dict.iteritems():
- if value > 1:
-  print key,value
+for key, value in dup_dict.iteritems():
+ if len(value) > 1:
+  print key,len(value)
   print "Final row: ", deduped_dict[key][1][0], deduped_dict[key][1][1], deduped_dict[key][1][5], deduped_dict[key][1][6], deduped_dict[key][1][8], deduped_dict[key][1][9], deduped_dict[key][1][10]
   print "Duplicate rows were: ", len(dup_dict[key])
   for i in range (0, len(dup_dict[key])):
    print "Line Number:", dup_dict[key][i][0],":", dup_dict[key][i][1][0], dup_dict[key][i][1][1], dup_dict[key][i][1][5], dup_dict[key][i][1][6], dup_dict[key][i][1][8], dup_dict[key][i][1][9]
   print "-------------------------------------"
   final_counter += 1
-  if value > max_counter:
-   max_counter = value
+  if len(value) > max_counter:
+   max_counter = len(value)
 
 print "Number of duplicate sets: ", final_counter
 print "Max number of duplicates in a set: ", max_counter
 print "Reached line number", linecounter
 
-#with open("cleaned.csv","wb") as csvfileout:
-# writer = csv.writer(csvfileout)
+with open("cleaned.csv","wb") as csvfileout:
+ writer = csv.writer(csvfileout)
+ conflictcounter = 0
+ for key,value in master_lines.iteritems():
+  if conflict_dict.get(key):
+   if len(conflict_dict[key]) > 0:
+    conflictcounter += 1
+  writer.writerow(value)
+ print conflictcounter,"rows have conflictin values"
